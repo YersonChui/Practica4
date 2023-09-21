1b2ffc774019da24462221738bf550a5a562caf2
YersonChui <142272272+YersonChui@users.noreply.github.com>
2023-09-21 19:16:59 -0400

    Update ejercicio 04_03.cpp

--- ejercicio 04_03.cpp
+++ ejercicio 04_03.cpp
@@ -1,7 +1,7 @@
-// Fecha creaci髇: 17/09/2023
-// Fecha modificaci髇: 18/09/2023
-// N鷐ero de ejericio: 3
-// Problema planteado: Leer los datos de doce personas como son: nombre, edad, estatura, peso, posteriormente indicar cu醠 es el nombre de la persona de menor edad, el promedio de las estaturas y el
+// Fecha creaci贸n: 17/09/2023
+// Fecha modificaci贸n: 18/09/2023
+// N煤mero de ejericio: 3
+// Problema planteado: Leer los datos de doce personas como son: nombre, edad, estatura, peso, posteriormente indicar cu谩l es el nombre de la persona de menor edad, el promedio de las estaturas y el
 // de los pesos. (realizar con estructuras)
 #include <iostream>
 #include <string>
@@ -13,8 +13,6 @@ struct Persona {
     float estatura;
     float peso;
 };// estructura que presenta los datos de la persona
-
-// Funci髇 que lee los datos de una persona por teclado y los almacena en una estructura
 void leer_persona(Persona&p) {
 
     cout << "Ingrese el nombre: ";cin >> p.nombre;
@@ -23,45 +21,32 @@ void leer_persona(Persona&p) {
     cout << "Ingrese el peso: ";cin >> p.peso;
 }// funcion que lee a la persona
 
-// Funci髇 que muestra los datos de una persona por pantalla
+// Funci贸n que muestra los datos de una persona por pantalla
 void mostrar_persona(Persona p) {
-    // Mostrar el nombre de la persona
     cout << "Nombre: " << p.nombre << endl;
-    // Mostrar la edad de la persona
     cout << "Edad: " << p.edad << endl;
-    // Mostrar la estatura de la persona
     cout << "Estatura: " << p.estatura << endl;
-    // Mostrar el peso de la persona
     cout << "Peso: " << p.peso << endl;
 }
 
-// Funci髇 que calcula el promedio de un arreglo de n鷐eros reales
+// Funci贸n que calcula el promedio de un arreglo de n煤meros reales
 float promedio(float arr[], int n) {
-    // Declarar una variable para almacenar la suma de los elementos del arreglo
     float suma = 0;
-    // Recorrer el arreglo y sumar cada elemento a la variable suma
     for (int i = 0; i < n; i++) {
         suma += arr[i];
     }
-    // Devolver la suma dividida entre el n鷐ero de elementos del arreglo
     return suma / n;
 }
 
 int main() {
-    // Declarar una constante para representar el n鷐ero de personas
     const int N = 12;
-    // Declarar un arreglo de estructuras para almacenar los datos de las personas
     Persona personas[N];
-    // Declarar un arreglo para almacenar las estaturas de las personas
     float estaturas[N];
-    // Declarar un arreglo para almacenar los pesos de las personas
     float pesos[N];
 
-    // Recorrer el arreglo de personas y leer los datos por teclado para cada una
     for (int i = 0; i < N; i++) {
         cout << "Ingrese los datos de la persona " << i + 1 << ":" << endl;
         leer_persona(personas[i]);
-        // Almacenar la estatura y el peso en los arreglos correspondientes
         estaturas[i] = personas[i].estatura;
         pesos[i] = personas[i].peso;
         cout << endl;
