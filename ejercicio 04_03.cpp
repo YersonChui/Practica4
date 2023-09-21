f3e7154322b62f92b54c2caf8c45559decfa2e42
YersonChui <142272272+YersonChui@users.noreply.github.com>
2023-09-21 19:17:39 -0400

    Update ejercicio 04_03.cpp

--- ejercicio 04_03.cpp
+++ ejercicio 04_03.cpp
@@ -1,5 +1,5 @@
 // Fecha creación: 17/09/2023
-// Fecha modificación: 18/09/2023
+// Fecha modificación: 21/09/2023
 // Número de ejericio: 3
 // Problema planteado: Leer los datos de doce personas como son: nombre, edad, estatura, peso, posteriormente indicar cuál es el nombre de la persona de menor edad, el promedio de las estaturas y el
 // de los pesos. (realizar con estructuras)
